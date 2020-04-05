0b7bb55578ef40b5fc168d26ba477426bf86cd01
pyprajwal <61958453+pyprajwal@users.noreply.github.com>
2020-04-05 21:56:15 +0545

    Add files via upload

---
+++ 2d lists & nested loops.py
@@ -0,0 +1,14 @@
+#2d lists & nested loops
+
+number_grid = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0]
+]
+
+#print(number_grid[2][1])
+
+for row in number_grid:
+    for col in row:
+        print (col)
---
+++ day 3.py
@@ -0,0 +1,32 @@
+'''character_name = "John"
+character_age = 35
+is_Male = True
+print("There was a " + character_name + ",")
+print("he was " + character_age +"year old")
+
+character_name = "Mike"
+print(""+ character_name +" is happy man")
+print("he didn't like to be "+ character_age +"")
+'''
+
+'''
+phrase = "Giraffe Academy"
+print((phrase+ " is cool").upper())
+print(phrase.index("Acad"))
+print(phrase.replace("Giraffe","Elephant"))
+'''
+
+'''from math import *
+
+my_num = -5
+print(pow(3,2))
+print(ceil(4.2))
+print(sqrt(4))
+print()
+'''
+
+'''
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello! " + name + " you are " +age )
+'''
